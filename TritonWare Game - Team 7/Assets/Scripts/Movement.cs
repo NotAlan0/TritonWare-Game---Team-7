d35b17fe0d23edd36f48dc55ebad8afcd44c4d01
NotAlan0 <notalan52@gmail.com>
2021-10-12 19:58:04 -0700

    Added movement
    Added movement, some basic scenery, folders, and top down camera

---
+++ TritonWare Game - Team 7/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Movement : MonoBehaviour
+{
+    public float walkingSpeed = 10f;
+    public float runningSpeed = 10f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
