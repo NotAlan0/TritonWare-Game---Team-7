ff36c34a1437e3c481995af347b5e883d5eb9cdf
Ripslash <92413947+Ripslash@users.noreply.github.com>
2021-10-18 22:28:25 -0700

    Update Transluscent.cs

--- TritonWare Game - Team 7/Assets/Scripts/Transluscent.cs
+++ TritonWare Game - Team 7/Assets/Scripts/Transluscent.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class Transluscent : MonoBehaviour
 {
     public GameObject hittable;
+    MeshRenderer storage;
     void Update()
     {
         RaycastHit hit;
-        Color storage;
         Color myNewColor;
         if (Physics.Raycast(transform.position, transform.forward, out hit, 100.0f))
         {
             hittable = hit.transform.gameObject;
-            if (hittable.gameObject.GetComponent<MeshRenderer>() != null) {
+            if (hittable.gameObject.GetComponent<MeshRenderer>() != null)
+            {
 
                 myNewColor = hittable.GetComponentInChildren<MeshRenderer>().material.color;
                 if (hit.transform.tag == "Block")
                 {
-                    storage = hittable.gameObject.GetComponentInChildren<MeshRenderer>().material.color;
+                    storage = hittable.gameObject.GetComponentInChildren<MeshRenderer>();
                     Debug.Log(storage);
                     myNewColor.a = 0.5f;
-                    hittable.gameObject.GetComponentInChildren<MeshRenderer>().material.color = myNewColor;
-                    Debug.Log(hittable.gameObject.GetComponentInChildren<MeshRenderer>().material);
+                    storage.material.color = myNewColor;
+                    Debug.Log(storage.material);
                 }
-                else { storage = myNewColor; }
-                if(storage != myNewColor) {
+                else { storage.material.color = myNewColor; }
+                if (storage.material.color != myNewColor)
+                {
                     myNewColor.a = 1f;
-                    storage = myNewColor;
+                    storage.material.color = myNewColor;
                 }
             }
         }
-
-
     }
 }
