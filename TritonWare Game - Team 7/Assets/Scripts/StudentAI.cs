879a8197f6c7222c233b92ffbae99a9e040e9df1
Boris Ryabov <stonks33@gmail.com>
2021-10-20 14:48:50 -0700

    Student AI fixed
    no more stupid floats >:((((((

--- TritonWare Game - Team 7/Assets/Scripts/StudentAI.cs
+++ TritonWare Game - Team 7/Assets/Scripts/StudentAI.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,35 +8,37 @@ public class StudentAI : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
 
-    public Vector3 target;
+    public UnityEngine.Vector3 target;
 
     [SerializeField]
-    private Vector3 currentPosition;
+    private UnityEngine.Vector3 currentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
         navMeshAgent.speed = 2;
-        target = new Vector3(transform.position.x + 1, 1.083333f, transform.position.z);
+
+        Wander();
+
     }
 
     // Update is called once per frame
     void Update()
     { 
-        currentPosition = transform.position;
-
-        if (currentPosition == target)
+        // Doesn't depend on actuall position but rather the distance between two points
+        if ((transform.position - target).magnitude <= 0.05f)
         {
             Wander();
         }
 
         navMeshAgent.SetDestination(target);
+
     }
 
     void Wander()
     {
-        Vector3 nextTarget = new Vector3(UnityEngine.Random.Range(-30, 30),transform.position.y, UnityEngine.Random.Range(-30, 30));
+        UnityEngine.Vector3 nextTarget = new UnityEngine.Vector3(UnityEngine.Random.Range(-30, 30), transform.position.y, UnityEngine.Random.Range(-30, 30));
         target = nextTarget;
     }
 }
